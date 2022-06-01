07850900d3284ff7ad0310c2141b8b171155011a
alessandro camargo <acamargodias@terra.com.br>
2022-06-01 08:56:21 -0300

    Adicionado CSS-Responsividade ok

--- main.js
+++ main.js
@@ -6,20 +6,28 @@ function buscarCep() {
     ajax.send();
 
     ajax.onload = function() {
-    document.getElementById('texto').innerHTML = this.responseText;
+
     
     // TRASNFORMEI TEXTO EM OBJETO
     let obj = JSON.parse(this.responseText);
 
+
+    document.getElementById('place').value = `${obj.logradouro}`
+    document.getElementById('district').value = `${obj.bairro}`
+    document.getElementById('city').value = `${obj.localidade}`
+    document.getElementById('state').value = `${obj.uf}`
+
+}
+}
+
+
+
+    // document.getElementById('texto').innerHTML = this.responseText;
+
     // PEGUEI VALORES
-    let logradouro = obj.logradouro;
-    let bairro = obj.bairro;
-    let cidade = obj.localidade;
-    let uf = obj.uf;
+    // let logradouro = obj.logradouro;
+    // let bairro = obj.bairro;
+    // let cidade = obj.localidade;
+    // let uf = obj.uf;
 
-    document.getElementById('texto').innerHTML = `Logradouro: ${logradouro}<br>Bairro: ${bairro} 
-    <br> Cidade: ${cidade} <br> UF:${uf}`
-}
-
-
-}
+    // document.getElementById('texto').innerHTML = `${logradouro}`
