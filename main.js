3c28ef1bddedc663493e88cdbd2bb0fb498292e3
Alessandro Camargo <acamargodias@terra.com.br>
2022-12-06 22:23:56 -0300

    js att

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
-function buscarCep() {
-    let campoCep = document.getElementById('cep').value;
+function loadCep() {
+  const resultCep = document.getElementById("cep").value;
+  fetch(`https://viacep.com.br/ws/${resultCep}/json/`)
+    .then((response) => response.json())
+    .then(function (json) {
+      console.log(json);
+      document.getElementById("place").value = `${json.logradouro}`;
+      document.getElementById("district").value = `${json.bairro}`;
+      document.getElementById("city").value = `${json.localidade}`;
+      document.getElementById("state").value = `${json.uf}`;
+    })
+    .catch(() => alert(`${resultCep} é um formato de CEP inválido.`));
+}
 
-    const ajax = new XMLHttpRequest();
-    ajax.open('GET', 'https://viacep.com.br/ws/' + campoCep + '/json/');
-    ajax.send();
+function clearValues() {
+  document.getElementById("cep").value = "";
+  document.getElementById("place").value = "";
+  document.getElementById("district").value = "";
+  document.getElementById("city").value = "";
+  document.getElementById("state").value = "";
+  document.getElementById("texto").innerHTML = "";
+}
 
-    ajax.onload = function() {
+function loadCep() {
+  let campoCep = document.getElementById("cep").value;
 
-    
+  const ajax = new XMLHttpRequest();
+  ajax.open("GET", "https://viacep.com.br/ws/" + campoCep + "/json/");
+  ajax.send();
+
+  ajax.onload = function () {
     // TRASNFORMEI TEXTO EM OBJETO
     let obj = JSON.parse(this.responseText);
 
-
-    document.getElementById('place').value = `${obj.logradouro}`
-    document.getElementById('district').value = `${obj.bairro}`
-    document.getElementById('city').value = `${obj.localidade}`
-    document.getElementById('state').value = `${obj.uf}`
-
+    document.getElementById("place").value = `${obj.logradouro}`;
+    document.getElementById("district").value = `${obj.bairro}`;
+    document.getElementById("city").value = `${obj.localidade}`;
+    document.getElementById("state").value = `${obj.uf}`;
+  };
 }
-}
-
-
-
-    // document.getElementById('texto').innerHTML = this.responseText;
-
-    // PEGUEI VALORES
-    // let logradouro = obj.logradouro;
-    // let bairro = obj.bairro;
-    // let cidade = obj.localidade;
-    // let uf = obj.uf;
-
-    // document.getElementById('texto').innerHTML = `${logradouro}`
