bc54e6cd4de81f133a75119d84d9133e6eefe9e1
Alessandro Camargo <acamargodias@terra.com.br>
2022-12-07 22:17:09 -0300

    testes API

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-function loadCep() {
+async function loadCep() {
   const resultCep = document.getElementById("cep").value;
-  fetch(`https://viacep.com.br/ws/${resultCep}/json/`)
-    .then((response) => response.json())
-    .then(function (json) {
-      console.log(json);
-      document.getElementById("place").value = `${json.logradouro}`;
-      document.getElementById("district").value = `${json.bairro}`;
-      document.getElementById("city").value = `${json.localidade}`;
-      document.getElementById("state").value = `${json.uf}`;
-    })
-    .catch(() => alert(`${resultCep} é um formato de CEP inválido.`));
+  let req = await fetch(`https://viacep.com.br/ws/${resultCep}/json/`);
+  let response = await req.json();
+
+  // .then((response) => response.json())
+  // .then(function (json) {
+  document.getElementById("place").value = `${response.logradouro}`;
+  document.getElementById("district").value = `${response.bairro}`;
+  document.getElementById("city").value = `${response.localidade}`;
+  document.getElementById("state").value = `${response.uf}`;
+  // })
+  // .catch(() => alert(`${resultCep} é um formato de CEP inválido.`));
 }
 
 function clearValues() {
